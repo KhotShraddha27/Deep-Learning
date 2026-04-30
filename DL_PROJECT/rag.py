f20c43c86d26cb7c7bf9422068a76ea367ad2c15
Shraddha Khot <khotshraddha27@gmail.com>
2026-04-30 16:20:32 +0530

    Langchain & RAG Project :STUDYBOT

---
+++ DL_PROJECT/rag.py
@@ -0,0 +1,70 @@
+import os
+from langchain_community.document_loaders import PyPDFLoader
+from langchain_text_splitters import RecursiveCharacterTextSplitter
+from langchain_huggingface import HuggingFaceEmbeddings
+from langchain_community.vectorstores import FAISS
+from langchain_groq import ChatGroq
+from langchain_core.prompts import ChatPromptTemplate
+
+# 📁 LOCAL DATA FOLDER
+PDF_FOLDER = "data"
+
+def load_pdfs():
+    docs = []
+    
+    for file in os.listdir(PDF_FOLDER):
+        if file.endswith(".pdf"):
+            path = os.path.join(PDF_FOLDER, file)
+            loader = PyPDFLoader(path)
+            pages = loader.load()
+
+            for p in pages:
+                p.metadata["source"] = file
+
+            docs.extend(pages)
+
+    return docs
+
+
+def create_rag():
+    documents = load_pdfs()
+
+    splitter = RecursiveCharacterTextSplitter(chunk_size=400, chunk_overlap=50)
+    chunks = splitter.split_documents(documents)
+
+    embeddings = HuggingFaceEmbeddings(
+        model_name="sentence-transformers/all-MiniLM-L6-v2"
+    )
+
+    vectorstore = FAISS.from_documents(chunks, embeddings)
+
+    retriever = vectorstore.as_retriever(search_kwargs={"k": 5})
+
+    llm = ChatGroq(model="llama-3.3-70b-versatile")
+
+    prompt = ChatPromptTemplate.from_template("""
+You are a helpful student assistant.
+
+Answer clearly using the given context.
+If not found, say: "Not in documents".
+
+Context:
+{context}
+
+Question:
+{question}
+""")
+
+    return retriever, llm, prompt
+
+
+def ask_question(query, retriever, llm, prompt):
+    docs = retriever.invoke(query)
+
+    context = "\n\n".join([d.page_content for d in docs])
+
+    response = llm.invoke(
+        prompt.format(context=context, question=query)
+    )
+
+    return response.content
